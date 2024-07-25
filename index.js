c3cc2fd15667337b6993a5099f916df669c2f5ae
Antonio Ramirez <antonioramirez9614@gmail.com>
2024-07-24 19:23:35 -0500

    finished what I could

--- index.js
+++ index.js
@@ -11,34 +11,73 @@ const getInfo = async() => {
   const artists = artistsJson.data;
   state.artists = artists;
   // state.artists.forEach(artist => console.log(artist.name))
+  // console.log(artists)
 }
 getInfo();
 
-const artistUL = document.createElement(`ul`);
+// grab the main element
 const main = document.querySelector(`main`)
+// create an unordered list
+const artistUL = document.createElement(`ul`);
+// attatch it to the main element
 main.appendChild(artistUL)
-artistUL.innerText = `click here!`
+// instruct to click
+artistUL.innerText = `(click here for artists!)`;
+artistUL.style.color = `#FFB6C1`
 
 artistUL.addEventListener(`click`, event => {
+  artistUL.innerText = ``;
+
   state.artists.forEach(artist => {
     const artistLI = document.createElement(`li`);
     artistUL.appendChild(artistLI);
-    // artistLI.innerText = artist.name;
+    const artistID = artist.id;
     artistLI.innerHTML =
     `
     <section>
     <img src="${artist.imageUrl}"></>
     <h3>${artist.name}</h3>
     <p>${artist.description}</p>
+    <button id="id${artist.id}">Remove</button>
     </section>
     `
-    artistLI.style.listStyle = `none`;
-    artistLI.style.marginTop = `20px`;
-    artistLI.style.width = `100%`;
-    // artistLI.style.border = `thin black solid`;
-    // artistLI.style.backgroundColor = `darkgreen`;
-    // artistLI.style.borderRadius = `40px`;
-
+    const deleteButton = artistLI.querySelector(`#id${artist.id}`);
+    deleteButton.addEventListener(`click`,(event) => {
+      event.preventDefault();
+      artistLI.remove();
+    });
 
   })
 })
+
+// const form = document.querySelector(`form`);
+// const addArtist = async(event) =>{
+
+//   event.preventDefault();
+//   let name = document.querySelector(`#artistName`).value;
+//   let description = document.querySelector(`#artistDescription`).value;
+//   let image = document.querySelector(`#artistImage`).value;
+
+//   const artistLI = document.createElement(`li`);
+//     artistUL.appendChild(artistLI);
+//     artistLI.innerHTML =
+//     `
+//     <section>
+//     <img src="${image}"></>
+//     <h3>${name}</h3>
+//     <p>${description}</p>
+//     <button class="deleteButton">Remove</button>
+//     </section>
+//     `
+//     const deleteButton = artistLI.querySelector(`.deleteButton`);
+//     let eleCount = 1;
+//     deleteButton.addEventListener(`click`,(event) => {
+//       event.preventDefault();
+//       artistLI.remove();
+//     });
+//     document.querySelector(`#artistName`).value = ``;
+//     document.querySelector(`#artistDescription`).value = ``;
+//     document.querySelector(`#artistImage`).value = ``;
+//     eleCount++;
+// }
+// form.addEventListener(`submit`, addArtist)
